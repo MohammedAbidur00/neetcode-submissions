39196a1d3034b83d57084d33c7cf7faa0592cbcd
MohammedAbidur00 <abidurrahman788909@gmail.com>
2026-08-11 12:16:03 +0100

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for (let x = 0; x < nums.length - 1; x++) {
+            for (let y = x + 1; y < nums.length; y++) {
+                if (nums[x] + nums[y] === target) {
+                    return [x, y]
+                }
+            }
+        }
+    }
+}
